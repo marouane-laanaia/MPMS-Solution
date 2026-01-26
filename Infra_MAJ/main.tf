d5046c5f79699a107cdb6118b6da0c37d4ee0349
Marouane Laanaia <marouane-laanaia@gmail.com>
2026-01-26 10:24:20 +0100

    ajouts des groupes de secu dans main.tf

--- Infra_MAJ/main.tf
+++ Infra_MAJ/main.tf
@@ -4,7 +4,7 @@ terraform {
       source  = "terraform-provider-openstack/openstack"
       version = "~> 1.53.0"
     }
-    time = { 
+    time = {
       source  = "hashicorp/time"
       version = "~> 0.9.1"
     }
@@ -25,23 +25,76 @@ data "openstack_compute_keypair_v2" "default" {
   name = "terraform"
 }
 
+# --- SECURITY GROUPS ---
+resource "openstack_networking_secgroup_v2" "k8s_secgroup" {
+  name        = "k8s-sg"
+  description = "Security group for Kubernetes cluster"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "ssh_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.k8s_secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_api_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.k8s_secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "internal_rule" {
+  description       = "Allow all internal traffic (Cilium VXLAN 8472/UDP, Etcd 2379-2380/TCP, Kubelet 10250/TCP, etc.)"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  remote_group_id   = openstack_networking_secgroup_v2.k8s_secgroup.id
+  security_group_id = openstack_networking_secgroup_v2.k8s_secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "nodeport_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 30000
+  port_range_max    = 32767
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.k8s_secgroup.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "icmp_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  security_group_id = openstack_networking_secgroup_v2.k8s_secgroup.id
+}
+
 # --- RÉSERVATION DES ADRESSES IP (Ports) ---
-# On enlève security_group_ids pour laisser OpenStack mettre le 'default' de ton projet
+# Utilisation des ports pour IP fixes
 resource "openstack_networking_port_v2" "cp_ports" {
   count      = 3
   name       = "port-k8s-cp${count.index + 1}"
   network_id = data.openstack_networking_network_v2.public.id
+
 }
 
 resource "openstack_networking_port_v2" "worker_ports" {
   count      = 3
   name       = "port-k8s-worker${count.index + 1}"
   network_id = data.openstack_networking_network_v2.public.id
+
 }
 
 # --- Délai d'attente pour le Control Plane 1 ---
 resource "time_sleep" "wait_for_cp_boot" {
-  create_duration = "10m" 
+  create_duration = "10m"
   depends_on      = [openstack_compute_instance_v2.cp[0]]
 }
 
@@ -66,19 +119,24 @@ resource "openstack_compute_instance_v2" "cp" {
   key_pair    = data.openstack_compute_keypair_v2.default.name
   image_id    = data.openstack_images_image_v2.ubuntu.id
 
+
+
   network {
     port = openstack_networking_port_v2.cp_ports[count.index].id
   }
 
-  volume {
-    volume_id = openstack_blockstorage_volume_v3.cp_data[count.index].id
-  }
 
   user_data = count.index == 0 ? templatefile("${path.module}/cloud-init/cp1.yaml", {
     control_plane_ip = openstack_networking_port_v2.cp_ports[0].all_fixed_ips[0]
-  }) : templatefile("${path.module}/cloud-init/worker.yaml", {
+    }) : templatefile("${path.module}/cloud-init/worker.yaml", {
     control_plane_ip = openstack_networking_port_v2.cp_ports[0].all_fixed_ips[0]
   })
+}
+
+resource "openstack_compute_volume_attach_v2" "cp_volume_attach" {
+  count       = 3
+  instance_id = openstack_compute_instance_v2.cp[count.index].id
+  volume_id   = openstack_blockstorage_volume_v3.cp_data[count.index].id
 }
 
 # --- WORKER NODES ---
@@ -89,17 +147,22 @@ resource "openstack_compute_instance_v2" "worker" {
   key_pair    = data.openstack_compute_keypair_v2.default.name
   image_id    = data.openstack_images_image_v2.ubuntu.id
 
+
+
   network {
     port = openstack_networking_port_v2.worker_ports[count.index].id
   }
 
-  volume {
-    volume_id = openstack_blockstorage_volume_v3.worker_data[count.index].id
-  }
 
   user_data = templatefile("${path.module}/cloud-init/worker.yaml", {
     control_plane_ip = openstack_networking_port_v2.cp_ports[0].all_fixed_ips[0]
   })
+}
+
+resource "openstack_compute_volume_attach_v2" "worker_volume_attach" {
+  count       = 3
+  instance_id = openstack_compute_instance_v2.worker[count.index].id
+  volume_id   = openstack_blockstorage_volume_v3.worker_data[count.index].id
 }
 
 # --- JOINTURE CP (SSH via IP du port) ---
@@ -138,9 +201,9 @@ resource "null_resource" "worker_join" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("~/.ssh/id_ed25519")
-    host        = openstack_networking_port_v2.worker_ports[count.index].all_fixed_ips[0] 
+    host        = openstack_networking_port_v2.worker_ports[count.index].all_fixed_ips[0]
   }
-  
+
   provisioner "file" {
     source      = "~/.ssh/id_ed25519"
     destination = "/home/ubuntu/.ssh/id_ed25519"
